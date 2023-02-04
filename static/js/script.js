94cc14bb40bf1e0c1e794b223435ec57e5e844c4
Sizimon <siizmonsus@gmail.com>
2023-02-04 04:49:07 +0000

    FEAT: Add image popup modal functionality

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,19 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $('.myImg').each(function() {
+        $(this).click(function() {
+            $('#myModal').css('display', 'block')
+            $('#imgModal').attr('src', this.src)
+            $('.navbar').css('display', 'none')
+        })
+    })
+    $('.close').each(function() {
+        $(this).click(function() {
+            $('#myModal').css('display', 'none')
+            $('.navbar').css('display', 'flex')
+        })
+    })
+})
