df610ff2f6e3ee383aff8cb31553cf4e0cc0084c
Sizimon <siizmonsus@gmail.com>
2023-02-08 15:48:18 +0000

    Heroku adjustment

--- PrintStore/models.py
+++ PrintStore/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 FAVOURITE = ((0, 'Not Added'), (1, 'Added'))
 
 
-class FavouriteImage(models.Model):
+class favouriteImage(models.Model):
     # Image
     featured_image = models.CloudinaryField('image')
     favourite = models.IntegerField(choices=FAVOURITE, default=0)
