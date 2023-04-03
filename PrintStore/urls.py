610c3bda283e25a9e203898fb7da664c996ad9f9
Sizimon <siizmonsus@gmail.com>
2023-04-03 18:26:07 +0000

    Repeated for production section

--- PrintStore/urls.py
+++ PrintStore/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 
 # importing views from views.py
-from .views import index_view, gallery_view, contact_view, artsy_images_list, production_images_list, favourite_images_list, ArtsyFavourite
+from .views import index_view, gallery_view, contact_view, artsy_images_list, production_images_list, favourite_images_list, ArtsyFavourite, ProductionFavourite
 
 urlpatterns = [
     path('', index_view, name='index'),
@@ -12,5 +12,6 @@ urlpatterns = [
     path('artsy-latest', views.artsy_images_list.as_view(), name='artsy-latest'),
     path('production-latest', views.production_images_list.as_view(), name='production-latest'),
     path('favourites', views.favourite_images_list.as_view(), name='favourites'),
-    path('favourite/<slug:slug>', views.ArtsyFavourite.as_view(), name='artsy_favourite'),
+    path('artsy_favourite/<slug:slug>', views.ArtsyFavourite.as_view(), name='artsy_favourite'),
+    path('production_favourite/<slug:slug>', views.ProductionFavourite.as_view(), name='production_favourite'),
 ]
