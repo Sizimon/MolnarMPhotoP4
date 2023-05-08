190a50b9f46a06f2770289b967285b57aa4fdc63
Sizimon <siizmonsus@gmail.com>
2023-05-08 12:21:49 +0000

    Removed user features and site integration to optimise for a minimal photography portfolio

--- PrintStore/models.py
+++ PrintStore/models.py
@@ -2,19 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
-
-# FAVOURITE = ((False, 'Not Added'), (True, 'Added'))
-
 
 class Photos(models.Model):
     # Slug
     slug = models.SlugField(max_length=200, unique=True)
     # Image
     featured_image = CloudinaryField('image', default='placeholder')
-    # Favourite Status
-    # favourite = models.ManyToManyField(
-    #     User, related_name='favourited_photos', blank=True)
 
 
 class ArtsyPhotos(models.Model):
@@ -22,9 +15,6 @@ class ArtsyPhotos(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     # Image
     featured_image = CloudinaryField('image', default='placeholder')
-    # Favourite Status
-    # favourite = models.ManyToManyField(
-    #     User, related_name='artsy_photos', blank=True)
 
 
 class ProductionPhotos(models.Model):
@@ -32,7 +22,3 @@ class ProductionPhotos(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     # Image
     featured_image = CloudinaryField('image', default='placeholder')
-    # Favourite Status
-    # favourite = models.ManyToManyField(
-    #     User, related_name='production_photos', blank=True)
-    # favourite = models.IntegerField(choices=FAVOURITE, default=False)
--- PrintStore/views.py
+++ PrintStore/views.py
@@ -32,28 +32,6 @@ class artsy_images_list(generic.ListView):
     context_object_name = 'ArtsyPhotos'
 
 
-# class ArtsyDetail(View):
-#     def get(self, request, slug, *args, **kwargs):
-#         queryset = ArtsyPhotos.objects.all()
-#         photos = get_object_or_404(queryset, slug=slug)
-#         favourited = False
-#         if photos.favourite.filter(id=request.user.id).exists():
-#             favourited = True
-
-
-# class ArtsyFavourite(View):
-
-#     def post(self, request, slug):
-#         photo = get_object_or_404(ArtsyPhotos, slug=slug)
-
-#         if photo.favourite.filter(id=request.user.id).exists():
-#             photo.favourite.remove(request.user)
-#         else:
-#             photo.favourite.add(request.user)
-
-#         return HttpResponseRedirect(reverse(request.path, args=[slug]))
-
-
 # PRODUCTION FEATURES
 
 class production_images_list(generic.ListView):
@@ -63,50 +41,10 @@ class production_images_list(generic.ListView):
     context_object_name = 'ProductionPhotos'
 
 
-# class ProductionDetail(View):
-#     def get(self, request, slug, *args, **kwargs):
-#         queryset = ProdcutionPhotos.objects.all()
-#         photos = get_object_or_404(queryset, slug=slug)
-#         favourited = False
-#         if photos.favourite.filter(id=request.user.id).exists():
-#             favourited = True
-
-
-# class ProductionFavourite(View):
-
-#     def post(self, request, slug):
-#         photo = get_object_or_404(ProductionPhotos, slug=slug)
-
-#         if photo.favourite.filter(id=request.user.id).exists():
-#             photo.favourite.remove(request.user)
-#         else:
-#             photo.favourite.add(request.user)
-
-#         return HttpResponseRedirect(reverse(request.path, args=[slug]))
-
-
 # FAVOURITE FEATURES
 
 class favourite_images_list(generic.ListView):
     model = Photos
-    queryset = Photos.objects.filter(favourite=True)
+    queryset = Photos.objects.all()
     template_name = 'favourites.html'
     context_object_name = 'photos'
-
-
-# class PhotoFavourite(View):
-
-#     def photo(self, request, slug):
-#         photo = get_object_or_404(Photos, slug=slug)
-
-#         if photo.favourite.filter(id=request.user.id).exists():
-#             photo.favourite.update(request.user)
-#         else:
-#             photo.favourite.add(request.user)
-
-#         return HttpResponseRedirect(reverse(request.path, args=[slug]))
-
-
-
-
-
